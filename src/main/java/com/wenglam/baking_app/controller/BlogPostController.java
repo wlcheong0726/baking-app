810b29705dc5d773c718197942f92478d663fc94
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-09-05 18:15:17 +0100

    BlogPostController - updated formatting

--- src/main/java/com/wenglam/baking_app/controller/BlogPostController.java
+++ src/main/java/com/wenglam/baking_app/controller/BlogPostController.java
@@ -13,17 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1/blogposts")
 @RequiredArgsConstructor // lombok - generate constructors
 // @CrossOrigin(origins = "http://localhost:5173")
 public class BlogPostController {
-    
+
     private final IBlogPostService blogPostService;
 
     @GetMapping
