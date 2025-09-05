fbccd9d053be6e8fb31350a6f6c60b37558acd62
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-09-05 14:22:11 +0100

    BlogPostController - added getmapping for getpostbyid + endpoint + getBlogPostById method

--- src/main/java/com/wenglam/baking_app/controller/BlogPostController.java
+++ src/main/java/com/wenglam/baking_app/controller/BlogPostController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,12 +34,23 @@ public class BlogPostController {
         return blogPostService.getAllBlogPosts();
     }
 
+    @GetMapping("/blogpost/{id}")
+    public ResponseEntity<?> getBlogPostById(@PathVariable Long id) {
+        BlogPost blogPost = blogPostService.getBlogPostById(id);
+
+        if (blogPost == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(blogPost);
+        }
+    }
+
     @PostMapping
     public BlogPost createBlogPost(@RequestBody BlogPost blogPost) {
         return blogPostService.createBlogPost(blogPost);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/blogpost/{id}")
     public BlogPost editBlogPost(@PathVariable Long id, @RequestBody BlogPost updatedBlogPost) {
         return blogPostService.updateBlogPost(id, updatedBlogPost);
     }
