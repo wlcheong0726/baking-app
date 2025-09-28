a60fab4959d46be3d3606ea5e50c4fc5eabc84d9
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-09-28 14:37:11 +0100

    WebConfig - added @Configuration; added comments

--- src/main/java/com/wenglam/baking_app/config/WebConfig.java
+++ src/main/java/com/wenglam/baking_app/config/WebConfig.java
@@ -1,20 +1,21 @@
 package com.wenglam.baking_app.config;
 
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer{
 
     @Value("${app.upload.dir:uploads}")
     private String uploadDir;
 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-        String location = "file:" + System.getProperty("user.dir") + "/" + uploadDir + "/";
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations(location) // "file:uploads/"
+        String location = "file:" + System.getProperty("user.dir") + "/" + uploadDir + "/"; //System.getProperty("user.dir") = project root (JVM working directory)
+        
+        registry.addResourceHandler("/uploads/**") // Define the URL pattern to serve static files
+                .addResourceLocations(location) // "file:$projectpath/uploads/"
                 .setCachePeriod(3600); // Cache for 1 hour
     }
-    
 }
