7bf634dc7d26e390694f0d3d37b2557d938376d3
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-10-01 13:32:17 +0100

    BlogPost - deleted @ColumnDefault(CURRENT_TIMESTAMP) for createdAt as it depends on DB, using @CreationTimestamp and @UpdateTimestamp - Spring - created in Java; deleted @PrePersist for creation time as is not needed anymore

--- src/main/java/com/wenglam/baking_app/entity/BlogPost.java
+++ src/main/java/com/wenglam/baking_app/entity/BlogPost.java
@@ -3,13 +3,14 @@ package com.wenglam.baking_app.entity;
 import java.time.Instant;
 
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -35,30 +36,19 @@ public class BlogPost {
     @Column(name = "author", nullable = false, length = 50)
     private String author;
 
-    @ColumnDefault("CURRENT_TIMESTAMP") // Default value for created_at column - Hibernate annotation
-    // Database will automatically set this value to the current timestamp when a
-    // new row is inserted
-    // only works when generating schema - not during runtime when persisting an
-    // entity
+    @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
 
+    @UpdateTimestamp
     @ColumnDefault("NULL")
     @Column(name = "updated_at")
     private Instant updatedAt;
-
+    
     @ColumnDefault("NULL")
     @Column(name = "updated_by")
     private String updatedBy;
 
     @Column(name = "image_url", length = 500)
     private String imageUrl;
-
-    // This method is called before the entity is persisted to the database
-    @PrePersist
-    protected void onCreate() {
-        if (this.createdAt == null) {
-            this.createdAt = Instant.now();
-        }
-    }
 }
