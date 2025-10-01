429add6162395d57579c63fac190b4278be0aebe
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-10-01 14:31:53 +0100

    BlogPostCreateData, BlogPostUpdateData - added @NoArgsConstructor, @EqualsAndHashCode and @ToString - last two callSuper

--- src/main/java/com/wenglam/baking_app/dto/BlogPostUpdateData.java
+++ src/main/java/com/wenglam/baking_app/dto/BlogPostUpdateData.java
@@ -1,41 +1,16 @@
 package com.wenglam.baking_app.dto;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class BlogPostUpdateData extends BlogPostBaseData {
     private String imageUrl;
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((imageUrl == null) ? 0 : imageUrl.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        BlogPostUpdateData other = (BlogPostUpdateData) obj;
-        if (imageUrl == null) {
-            if (other.imageUrl != null)
-                return false;
-        } else if (!imageUrl.equals(other.imageUrl))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "BlogPostUpdateData [imageUrl=" + imageUrl + ", title=" + title + ", content=" + content + ", author="
-                + author + ", imageFile=" + imageFile + "]";
-    }
 }
