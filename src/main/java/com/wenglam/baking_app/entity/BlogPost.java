296f2a2cf18513208da1bdb6163c6839bbc10be4
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-10-01 14:32:35 +0100

    BlogPost - added @NoArgsConstructor, @EqualsAndHashCode and @ToString

--- src/main/java/com/wenglam/baking_app/entity/BlogPost.java
+++ src/main/java/com/wenglam/baking_app/entity/BlogPost.java
@@ -12,11 +12,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 @Entity
 @Table(name = "blog_post")
 public class BlogPost {
