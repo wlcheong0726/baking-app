b7d09bf08d66153f5cb233aac69796aee0055cc9
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-10-09 14:48:59 +0100

    ImageFileStorageServiceTest - new test class with unit tests for successful and unsuccessful storage of images

---
+++ src/test/java/com/wenglam/baking_app/ImageFileStorageServiceTest.java
@@ -0,0 +1,93 @@
+package com.wenglam.baking_app;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.wenglam.baking_app.service.ImageFileStorageService;
+
+@ExtendWith(MockitoExtension.class)
+public class ImageFileStorageServiceTest {
+
+    @TempDir
+    Path tempDir;
+
+    @Mock
+    MultipartFile multipartFileMock;
+
+    @InjectMocks
+    private ImageFileStorageService imageFileStorageService;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        ReflectionTestUtils.setField(imageFileStorageService, "uploadDir", tempDir.toString());
+        ReflectionTestUtils.setField(imageFileStorageService, "publicBaseUrl", "http://localhost:8080");
+        ReflectionTestUtils.setField(imageFileStorageService, "allowedTypes", List.of("image/jpeg", "image/png"));
+        ReflectionTestUtils.setField(imageFileStorageService, "maxFileSize", "5242880"); // 5MB in bytes
+
+        imageFileStorageService.init();
+    }
+
+
+    @Test
+    void store_ValidImage_Returns_fullImageUrl() throws IOException {
+        when(multipartFileMock.isEmpty()).thenReturn(false);
+        when(multipartFileMock.getSize()).thenReturn(1024L);
+        when(multipartFileMock.getContentType()).thenReturn("image/png");
+        when(multipartFileMock.getOriginalFilename()).thenReturn("test_image.png");
+        when(multipartFileMock.getInputStream()).thenReturn(new ByteArrayInputStream("fake image".getBytes()));
+
+        String imageUrl = imageFileStorageService.store(multipartFileMock);
+
+        assertNotNull(imageUrl);
+        assertTrue(imageUrl.startsWith("http://localhost:8080/uploads/"));
+        assertTrue(imageUrl.endsWith("_test_image.png"));
+    }
+
+    @Test
+    void store_ValidImage_Returns_relativeImageUrl() throws IOException {
+        ReflectionTestUtils.setField(imageFileStorageService, "publicBaseUrl", null);
+        when(multipartFileMock.isEmpty()).thenReturn(false);
+        when(multipartFileMock.getSize()).thenReturn(1024L);
+        when(multipartFileMock.getContentType()).thenReturn("image/png");
+        when(multipartFileMock.getOriginalFilename()).thenReturn("test_image.png");
+        when(multipartFileMock.getInputStream()).thenReturn(new ByteArrayInputStream("fake image".getBytes()));
+
+        String imageUrl = imageFileStorageService.store(multipartFileMock);
+
+        assertNotNull(imageUrl);
+        assertTrue(imageUrl.startsWith("/uploads/"));
+        assertTrue(imageUrl.endsWith("_test_image.png"));
+    }
+
+    @Test
+    void store_ImageSizeTooLarge_Throws_IllegalArgumentException() throws IOException {
+        when(multipartFileMock.isEmpty()).thenReturn(false);
+        when(multipartFileMock.getSize()).thenReturn(100000000L);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> imageFileStorageService.store(multipartFileMock));
+        assertTrue(exception.getMessage().contains("Image size too large"));
+    }
+
+    @Test
+    void store_InvalidContentType_Throws_IllegalArgumentException() throws IOException {
+        when(multipartFileMock.isEmpty()).thenReturn(false);
+        when(multipartFileMock.getSize()).thenReturn(1024L);
+        when(multipartFileMock.getContentType()).thenReturn("application/x-7z-compressed");
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> imageFileStorageService.store(multipartFileMock));
+        assertTrue(exception.getMessage().contains("Rejected image type"));
+    }
+    
+}
