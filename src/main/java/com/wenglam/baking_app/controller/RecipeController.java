383b285b3fc3028863b3eea228a057f31dbbc716
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-11-29 18:11:02 +0000

    RecipeController - new controller specifically for recipes - one endpoint currently to get dessert recipes

---
+++ src/main/java/com/wenglam/baking_app/controller/RecipeController.java
@@ -0,0 +1,26 @@
+package com.wenglam.baking_app.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wenglam.baking_app.service.RecipeService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/recipes")
+@RequiredArgsConstructor
+public class RecipeController {
+    
+    private final RecipeService recipeService;
+
+    @GetMapping("/")
+    public ResponseEntity<?> getDessertRecipes() {
+        log.info("Received request to fetch dessert recipes");
+        return ResponseEntity.ok(recipeService.getDessertRecipes());
+    }
+}
