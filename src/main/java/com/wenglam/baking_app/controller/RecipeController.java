edd727fb0514bc187d6a81d3cb1c22e9cc4e3c0e
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-12-02 10:38:37 +0000

    RecipeController - updated getDessertRecipes to accept pagination and keyword request params

--- src/main/java/com/wenglam/baking_app/controller/RecipeController.java
+++ src/main/java/com/wenglam/baking_app/controller/RecipeController.java
@@ -1,8 +1,10 @@
 package com.wenglam.baking_app.controller;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wenglam.baking_app.service.impl.RecipeService;
@@ -18,9 +20,16 @@ public class RecipeController {
     
     private final RecipeService recipeService;
 
-    @GetMapping("/")
-    public ResponseEntity<?> getDessertRecipes() {
+    @GetMapping
+    public ResponseEntity<?> getDessertRecipes(@RequestParam(required = false, defaultValue = "1") int pageNo,
+                                                        @RequestParam(required = false, defaultValue = "20") int pageSize,
+                                                        @RequestParam(required = false, defaultValue = "asc") String sortDir,
+                                                        @RequestParam(required = false) String keyword
+                                                        ) {
         log.info("Received request to fetch dessert recipes");
-        return ResponseEntity.ok(recipeService.getDessertRecipes());
+
+        // add sort functionality later
+
+        return ResponseEntity.ok(recipeService.getDessertRecipes(keyword, PageRequest.of(pageNo-1, pageSize)));
     }
 }
