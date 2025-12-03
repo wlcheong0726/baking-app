36630bc27adf81669c95b6d6a204421d253d003c
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-12-03 22:08:22 +0000

    ApiNotSubscribedException - new exception class for tasty feign client'

---
+++ src/main/java/com/wenglam/baking_app/exception/ApiNotSubscribedException.java
@@ -0,0 +1,7 @@
+package com.wenglam.baking_app.exception;
+
+public class ApiNotSubscribedException extends RuntimeException {
+    public ApiNotSubscribedException(String message) {
+        super(message);
+    }
+}
