d71e51da9b76ecaae403c6a3d9a73b8a504fcce8
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-12-03 22:08:43 +0000

    TastyRecipeNotFoundException - new exception class for tasty feign client'

---
+++ src/main/java/com/wenglam/baking_app/exception/TastyRecipeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.wenglam.baking_app.exception;
+
+public class TastyRecipeNotFoundException extends RuntimeException {
+    public TastyRecipeNotFoundException(String message) {
+        super(message);
+    }
+    
+}
