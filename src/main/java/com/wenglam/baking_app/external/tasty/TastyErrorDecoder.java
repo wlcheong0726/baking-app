c914f92de283028cd2851ada1ae6162875ab8826
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-12-04 12:04:02 +0000

    TastyErrorDecoder - added logs to log method key and responses for development

--- src/main/java/com/wenglam/baking_app/external/tasty/TastyErrorDecoder.java
+++ src/main/java/com/wenglam/baking_app/external/tasty/TastyErrorDecoder.java
@@ -6,7 +6,9 @@ import com.wenglam.baking_app.exception.TastyRecipeNotFoundException;
 
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class TastyErrorDecoder implements ErrorDecoder {
 
     @Override
@@ -14,10 +16,13 @@ public class TastyErrorDecoder implements ErrorDecoder {
 
         switch (response.status()) {
             case 403:
+                log.info("Method Key: {}. Response: {}", methodKey, response.toString());
                 return new ApiNotSubscribedException("You are not subscribed to Tasty API: " + response.status());
             case 404:
+                log.info("Method Key: {}. Response: {}", methodKey, response.toString());
                 return new TastyRecipeNotFoundException("Tasty recipe not found: " + response.status());
             default:
+                log.error("Method Key: {}. Response: {}", methodKey, response.toString());
                 return new ExternalApiException(
                     "Tasty",
                     response.status(),
